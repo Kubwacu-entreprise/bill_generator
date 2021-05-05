1a09beab21eb77199f0e6138eceff386ab067a17
kalculata <nprincehuzaifa@gmail.com>
2021-05-05 09:52:46 +0200

    add fontawesome css file

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="lib/css.css">
+    <link rel="stylesheet" href="lib/font_awesome/all.min.css">
     <title>Bill Generator</title>
 </head>
 <body>
@@ -15,8 +16,12 @@
         </div>
     </header>
     <div class="body">
-        <div class="empty_message">
+        <div class="empty_bills_message">
+            <div></div>
             <h1>You haven't generated any payment invoice yet</h1>
+            <div>
+                <button>Invoice</button>
+            </div>
         </div>
     </div>
 </body>
