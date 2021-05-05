96bbd4d550220188c5855e48193407752c0b1acc
kalculata <nprincehuzaifa@gmail.com>
2021-05-05 09:15:38 +0200

    add global property and color variables in css file

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lib/css.css">
     <title>Bill Generator</title>
 </head>
 <body>
-    
+    <header>
+        <div><h1></h1></div>
+        <div></div>
+    </header>
 </body>
-</html>l
+</html>
