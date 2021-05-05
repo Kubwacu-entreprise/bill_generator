870205c323f5d1e2b6a7c47cad17bf0bd2faf490
kalculata <nprincehuzaifa@gmail.com>
2021-05-05 10:03:30 +0200

    add .gitignore

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="lib/css.css">
-    <link rel="stylesheet" href="lib/font_awesome/all.min.css">
+    <link rel="stylesheet" href="lib/font_awesome/css/all.min.css">
     <title>Bill Generator</title>
 </head>
 <body>
@@ -20,7 +20,7 @@
             <div></div>
             <h1>You haven't generated any payment invoice yet</h1>
             <div>
-                <button>Invoice</button>
+                <button><i class="fas fa-plus"></i> Invoice </button>
             </div>
         </div>
     </div>
